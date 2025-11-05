d7d796981b126c1cc8e569d0c0a262960b77e5b1
pshoxsanam2197 <pshoxsanam2197@gmail.com>
2025-11-05 18:06:21 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+# 1 -misol
+for i in range(1, 51):
+    print(i)
+    if i == 42:
+        break
+
+# 2 -misol
+for i in range(1, 21):
+    print(i)
+    if i % 2 == 0:
+        break
+
+# 3
+for i in range(1, 31):
+    print(i)
+    if i % 5 == 0:
+        break
+
+
+# 4
+for i in range(1, 16):
+    if i % 3 == 0:
+        break
+    print(i)
+
+# 5
+for i in range(1, 21):
+    print(i)
+    if i == 25:
+      break
